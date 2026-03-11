04ecf33d80cad49c846082c59cdfa972d31fe136
isnika <Ngkhanhhuyen67@gmail.com>
2026-03-11 19:40:39 +0700

    add note

---
+++ frontend/src/Data/books.ts
@@ -0,0 +1,19 @@
+export interface Book {
+  id: number
+  title: string
+  price: number
+  author: string
+}
+
+
+{/*import { books } from "../data/books";
+
+   function Home() {
+     return (
+       <div>
+         {books.map(book => (
+           <p key={book.id}>{book.title}</p>
+         ))}
+       </div>
+     );
+   }*/}
