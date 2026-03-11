0c10ad549e2e60ffae3cb1cc382d70c642a0aa15
isnika <Ngkhanhhuyen67@gmail.com>
2026-03-12 01:00:16 +0700

    add page profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Test from "./pages/test.tsx";
 import Layout from "./components/layout/Layout";
+import Profile from "./pages/Profile.tsx"
 
 
 function App() {
   return (
     <Layout>
-      <Test/>
+
     </Layout>
   );
 }
