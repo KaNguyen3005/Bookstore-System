db7d34acca68e846bea7c2839646c0265c275131
isnika <Ngkhanhhuyen67@gmail.com>
2026-04-09 20:10:10 +0700

    edit layout home

--- frontend/src/features/home/pages/Home/Home.tsx
+++ frontend/src/features/home/pages/Home/Home.tsx
@@ -20,17 +20,24 @@ function Home() {
 
   return (
     <div className="home-page">
-      <div className="section">
-        <HeroBanner books={homeData.topSellingBooks} />
-      </div>
-      <div className="section">
-        <HotSearchBooks books={homeData.hotSearchBooks} />
-      </div>
-      <div className="section">
-        <TopSellingBooks books={homeData.topSellingBooks} />
-      </div>
-      <div className="section">
-        <ExploreCategories />
+      <div className="container">
+
+        <div className="section">
+          <HeroBanner books={homeData.topSellingBooks} />
+        </div>
+
+        <div className="section">
+          <HotSearchBooks books={homeData.hotSearchBooks} />
+        </div>
+
+        <div className="section">
+          <TopSellingBooks books={homeData.topSellingBooks} />
+        </div>
+
+        <div className="section">
+          <ExploreCategories />
+        </div>
+
       </div>
     </div>
   );
